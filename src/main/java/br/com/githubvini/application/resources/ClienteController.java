cf0e4a3363a6f9cd8202f46b1e6f2ec4c33401e9
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2020-10-18 23:04:31 -0300

    Consultando por ID um cliente e caos nao exista o cliente é lançado uma exception 404

--- src/main/java/br/com/githubvini/application/resources/ClienteController.java
+++ src/main/java/br/com/githubvini/application/resources/ClienteController.java
@@ -5,6 +5,7 @@ import br.com.githubvini.application.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Classe responsável por ser o provedor e controlador da aplicação.
@@ -16,6 +17,13 @@ public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    @RequestMapping( value = "/{id}", method = RequestMethod.GET)
+    public Cliente findById( @PathVariable Integer id ) {
+        return clienteRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+    }
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
